7e1ccc1a5fbbd67e9507faffa93154711cfc509d
Ridho Maulana <edoin21@gmail.com>
2021-03-16 18:03:44 +0700

    Add remove console plugin

---
+++ plugins/remove-console.ts
@@ -0,0 +1,21 @@
+import { Plugin } from "vite";
+const fileRegex = /\.(ts|tsx)$/;
+
+export default function removeConsolePlugin(): Plugin {
+  return {
+    name: "remove-console",
+    apply: "build",
+
+    transform(src, id) {
+      if (fileRegex.test(id)) {
+        return {
+          code: src.replace(
+            /console.(log|debug|info|...|count)\([^)]+\);?/,
+            ""
+          ),
+          map: null, // provide source map if available
+        };
+      }
+    },
+  };
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import removeConsolePlugin from "./plugins/remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh(), removeConsolePlugin()],
+});
