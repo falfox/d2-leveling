b4f1ed6bc60f41746314dae1adefdf345a91ad2c
Ridho Maulana <edoin21@gmail.com>
2021-03-26 07:18:52 +0700

    Add more placement

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { parse } from "query-string";
 import React, { useEffect, useRef, useState } from "react";
 import { ChecklistsPanel } from "./components/checklists_panel";
 import { LoginWithBungie } from "./components/login_with_bungie";
+import { MadeBy } from "./components/made_by";
 import { MembershipBadge } from "./components/membership_badge";
 import { SingleCharacterPanel } from "./components/single_character_panel";
 import { CLASS_NAMES } from "./consts";
@@ -69,6 +70,7 @@ function App() {
         <div className="relative z-10">
           <LoginWithBungie />;
         </div>
+        <MadeBy />
       </div>
     );
 
@@ -88,7 +90,7 @@ function NewApp() {
   }, []);
 
   return (
-    <div className="antialiased">
+    <div className="relative antialiased">
       {!hasData ? (
         <div
           className="relative flex items-center justify-center w-full min-h-screen space-x-2 text-gray-900"
@@ -120,6 +122,7 @@ function NewApp() {
       ) : (
         <AppOld />
       )}
+      <MadeBy />
     </div>
   );
 }
@@ -373,34 +376,6 @@ function AppOld() {
               )}
             </AnimatePresence>
           </AnimateSharedLayout>
-        </div>
-      </div>
-      <div className="absolute inset-x-0 bottom-0">
-        <div className="flex items-center justify-start w-full py-2 text-white">
-          <span className="inline-flex items-center">
-            Made by{" "}
-            <a
-              href="https://discordapp.com/users/210939885002031105"
-              className="inline-flex items-center px-2 space-x-1 font-semibold"
-              target="_blank"
-            >
-              <svg
-                className="w-6 h-6 fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 245 240"
-              >
-                <path
-                  className="st0"
-                  d="M104.4 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1.1-6.1-4.5-11.1-10.2-11.1zm36.5 0c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1s-4.5-11.1-10.2-11.1z"
-                />
-                <path
-                  className="st0"
-                  d="M189.5 20h-134C44.2 20 35 29.2 35 40.6v135.2c0 11.4 9.2 20.6 20.5 20.6h113.4l-5.3-18.5 12.8 11.9 12.1 11.2 21.5 19V40.6c0-11.4-9.2-20.6-20.5-20.6zm-38.6 130.6s-3.6-4.3-6.6-8.1c13.1-3.7 18.1-11.9 18.1-11.9-4.1 2.7-8 4.6-11.5 5.9-5 2.1-9.8 3.5-14.5 4.3-9.6 1.8-18.4 1.3-25.9-.1-5.7-1.1-10.6-2.7-14.7-4.3-2.3-.9-4.8-2-7.3-3.4-.3-.2-.6-.3-.9-.5-.2-.1-.3-.2-.4-.3-1.8-1-2.8-1.7-2.8-1.7s4.8 8 17.5 11.8c-3 3.8-6.7 8.3-6.7 8.3-22.1-.7-30.5-15.2-30.5-15.2 0-32.2 14.4-58.3 14.4-58.3 14.4-10.8 28.1-10.5 28.1-10.5l1 1.2c-18 5.2-26.3 13.1-26.3 13.1s2.2-1.2 5.9-2.9c10.7-4.7 19.2-6 22.7-6.3.6-.1 1.1-.2 1.7-.2 6.1-.8 13-1 20.2-.2 9.5 1.1 19.7 3.9 30.1 9.6 0 0-7.9-7.5-24.9-12.7l1.4-1.6s13.7-.3 28.1 10.5c0 0 14.4 26.1 14.4 58.3 0 0-8.5 14.5-30.6 15.2z"
-                />
-              </svg>
-              <span>FalFox#4294</span>
-            </a>
-          </span>
         </div>
       </div>
     </div>
